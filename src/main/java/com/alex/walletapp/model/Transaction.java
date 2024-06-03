22eb9259b15b78fe3c610ba6d8f72405820fde78
kubicaaa <chabrowski31@gmail.com>
2024-06-03 08:49:35 +0200

    Docker implementation

--- src/main/java/com/alex/walletapp/model/Transaction.java
+++ src/main/java/com/alex/walletapp/model/Transaction.java
@@ -1,10 +1,7 @@
 package com.alex.walletapp.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
--- src/main/java/com/alex/walletapp/service/TransactionService.java
+++ src/main/java/com/alex/walletapp/service/TransactionService.java
@@ -1,15 +1,37 @@
 package com.alex.walletapp.service;
 
 import com.alex.walletapp.model.Transaction;
+import com.alex.walletapp.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface TransactionService {
-    public List<Transaction> findAll();
-    public Transaction findById(@PathVariable Integer id);
-    public Transaction save(Transaction transaction);
-    public boolean existsById(Integer id);
-    public void delete(@PathVariable Integer id);
+@Service
+public class TransactionService{
+    @Autowired
+    private TransactionRepository repository;
+
+    public List<Transaction> findAll() {
+        return repository.findAll();
+    }
+
+    public Transaction findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
+    }
+
+    public Transaction save(Transaction transaction){
+        return repository.save(transaction);
+    }
+
+    public boolean existsById(Integer id) {
+        return repository.existsById(id);
+    }
+
+    public void delete(@PathVariable Integer id) {
+        repository.delete(findById(id));
+    }
 }
